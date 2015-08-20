2e13374a36e30be63406bb28c783e3ab651f707a
ilyakuzovkin <ilya.kuzovkin@gmail.com>
2015-08-20 12:08:13 +0300

    Small updates to fourier preprocessing

--- Preprocessing/fourier.py
+++ Preprocessing/fourier.py
@@ -23,9 +23,10 @@ class Preprocessor:
         sfreq = 1000.0
         ws = rawdata.shape[0]
         binstep = int(sfreq / ws)
+        fftcutoff = 150
 
         # one bin should correspond to 1Hz regardless of windows size and sampling frequency
-        bins = [(start, start + binstep - 1) for start in range(0, ws / 2, binstep)]
+        bins = [(start, start + binstep - 1) for start in range(0, ws, binstep)]
 
         # transform data
         channels = rawdata.shape[1]
@@ -46,12 +47,13 @@ class Preprocessor:
             representation = []
             for bin in bins:
                 representation += [np.mean(freqspace[bin[0]:bin[1] + 1])]
+            representation = representation[0:fftcutoff]
 
             # append to current feature vector
             features = np.hstack((features, representation))
 
-        #plt.plot(features)
-        #plt.show()
+        plt.plot(features)
+        plt.show()
 
         return np.array(features)
 
