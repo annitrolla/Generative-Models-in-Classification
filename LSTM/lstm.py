9a9b70f5fe8a693093af9bcf5f77c71909ce543d
annitrolla <annitrolla@gmail.com>
2015-09-06 17:54:21 +0300

    LSTM for sliced data

--- LSTM/lstm.py
+++ LSTM/lstm.py
@@ -11,34 +11,20 @@ from keras.layers.core import Dense, Dropout, Activation
 from keras.layers.embeddings import Embedding
 from keras.layers.recurrent import LSTM, GRU
 from keras.datasets import imdb
+from DataNexus.datahandler import DataHandler
 
 batch_size = 32
 
 # read the data
 print("Reading data...")
-X_train = np.loadtxt('/storage/hpc_anna/GMiC/Data/ECoG/Competition_train_cnt_scaled.txt')
-y_train = np.loadtxt('/storage/hpc_anna/GMiC/Data/ECoG/Competition_train_lab_onezero.txt')
-#X_test = np.loadtxt('/storage/hpc_anna/GMiC/Data/ECoG/Competition_test_cnt_scaled.txt')
-#y_test = np.loadtxt('/storage/hpc_anna/GMiC/Data/ECoG/Competition_test_lab_onezero.txt')
-
-# reshape
-X_train = np.reshape(X_train, (278, 64, 3000))
-#X_test = np.reshape(X_test, (100, 64, 3000))
+dh = DataHandler("/storage/hpc_anna/GMiC/Data/ECoG/preprocessed")
+dh.load_train_data()
 
 # put features to be the last dimension
-X_train = np.transpose(X_train, (0, 2, 1))
+X_train = np.transpose(dh.trainval_data, (0, 2, 1))
 #X_test = np.transpose(X_test, (0, 2, 1))
 
-# split training and validation
-#shuffle = np.random.permutation(X_train.shape[0])
-#ntrain = int(X_train.shape[0] * 0.7)
-#X_val = X_train[shuffle[(ntrain + 1):]]
-#y_val = y_train[shuffle[(ntrain + 1):]]
-#X_train = X_train[shuffle[:ntrain]]
-#y_train = y_train[shuffle[:ntrain]]
-
 print('X_train shape:', X_train.shape)
-#print('X_val shape:', X_val.shape)
 #print('X_test shape:', X_test.shape)
 
 print('Build model...')
@@ -52,7 +38,7 @@ model.add(Activation('sigmoid'))
 model.compile(loss='binary_crossentropy', optimizer='adam', class_mode="binary")
 
 print("Train...")
-model.fit(X_train, y_train, batch_size=batch_size, nb_epoch=10, validation_split=0.7, show_accuracy=True)
+model.fit(X_train, dh.trainval_labels, batch_size=batch_size, nb_epoch=10, validation_split=0.3, show_accuracy=True)
 #score, acc = model.evaluate(X_test, y_test, batch_size=batch_size, show_accuracy=True)
 #print('Test score:', score)
 #print('Test accuracy:', acc)
