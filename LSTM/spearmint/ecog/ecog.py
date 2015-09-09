4c963797e2c6d374e52fea9892b766565990da6f
annitrolla <annitrolla@gmail.com>
2015-09-09 21:59:45 +0300

    spearmint for LSTM

--- LSTM/spearmint/ecog/ecog.py
+++ LSTM/spearmint/ecog/ecog.py
@@ -1,20 +1,45 @@
+from __future__ import absolute_import
+from __future__ import print_function
 import numpy as np
-import math
 
-def branin(x, y):
+from keras.preprocessing import sequence
+from keras.optimizers import SGD, RMSprop, Adagrad
+from keras.utils import np_utils
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation
+from keras.layers.embeddings import Embedding
+from keras.layers.recurrent import LSTM, GRU
+from keras.datasets import imdb
+from DataNexus.datahandler import DataHandler
+import numpy as np
+                                   
+def ecoglstm(lstmsize, dropout, optim):
+    batch_size = 32
 
-    result = np.square(y - (5.1/(4*np.square(math.pi)))*np.square(x) + 
-         (5/math.pi)*x - 6) + 10*(1-(1./(8*math.pi)))*np.cos(x) + 10
+    print("Reading data...")
+    dh = DataHandler("/storage/hpc_anna/GMiC/Data/ECoG/preprocessed")
+    dh.load_train_data()
+
+    # put features to be the last dimension
+    X_train = np.transpose(dh.trainval_data, (0, 2, 1))
     
-    result = float(result)
-    noise = np.random.normal() * 50.
-    
-    print 'Result = %f, noise %f, total %f' % (result, noise, result+noise)
-    #time.sleep(np.random.randint(60))
-    return result + noise
+    print("Building model...")
+    model = Sequential()
+    model.add(LSTM(64, lstmsize[0]))
+    model.add(Dropout(dropout[0]))
+    model.add(Dense(lstmsize[0], 1))
+    model.add(Activation('sigmoid'))
+
+    # try using different optimizers and different optimizer configs
+    print("Training...")
+    model.compile(loss='binary_crossentropy', optimizer=optim[0], class_mode="binary")
+    results = model.fit(X_train, dh.trainval_labels, batch_size=batch_size, nb_epoch=10, validation_split=0.3, show_accuracy=True)
+    result = results.history['val_acc'][-1]
+    print('Result = %f' % result)
+    return result
 
 # Write a function like this called 'main'
 def main(job_id, params):
-    print 'Anything printed here will end up in the output directory for job #%d' % job_id
-    print params
-    return branin(params['x'], params['y'])
+    print('Anything printed here will end up in the output directory for job #%d' % job_id)
+    print(params)
+    return ecoglstm(params['lstmsize'], params['dropout'], params['optim'])
