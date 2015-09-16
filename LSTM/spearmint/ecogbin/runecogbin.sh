0bf068f85d86bf9cc0889268a37cb1aae5211274
annitrolla <annitrolla@gmail.com>
2015-09-16 15:21:48 +0300

    changed runecog such that both users can run it

--- LSTM/spearmint/ecogbin/runecogbin.sh
+++ LSTM/spearmint/ecogbin/runecogbin.sh
@@ -4,7 +4,7 @@ source ~/Python/bin/activate
 source ~/.bash_profile
 pkill mongod
 sleep 10
-/home/hpc_kuz/Software/mongodb/mongodb-linux-x86_64-3.0.4/bin/mongod --fork --logpath ~/Research/Generative-Models-in-Classification/Results/ecogbin/log.txt --dbpath ~/Research/Generative-Models-in-Classification/Results/ecogbin
+"/home/"$USER"/Software/mongodb/bin/mongod" --fork --logpath ~/Research/Generative-Models-in-Classification/Results/Spearmint/ecogbin/log.txt --dbpath ~/Research/Generative-Models-in-Classification/Results/Spearmint/ecogbin
 cd ~/Software/Spearmint/spearmint
 ./cleanup.sh ~/Research/Generative-Models-in-Classification/Code/LSTM/spearmint/ecogbin/
 python main.py ~/Research/Generative-Models-in-Classification/Code/LSTM/spearmint/ecogbin/
