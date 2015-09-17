a3619f37a747921a4ba1acfc6f09069ab7693583
kuz <ilya.kuzovkin@gmail.com>
2015-09-17 15:36:42 +0300

    Runnable hybrid model scripts

--- RF-HMM/rfhmm_on_ecog.py
+++ RF-HMM/rfhmm_on_ecog.py
@@ -13,9 +13,10 @@ from sklearn.ensemble import RandomForestClassifier
 
 # parameters
 NSTATES = 20
-NITERS = 100
+NITERS = 30
 
 # load the data
+print "Reading data..."
 train_data = np.load("/storage/hpc_anna/GMiC/Data/ECoG/preprocessed/train_data.npy")
 train_labels = np.load("/storage/hpc_anna/GMiC/Data/ECoG/preprocessed/train_labels.npy")
 test_data = np.load("/storage/hpc_anna/GMiC/Data/ECoG/preprocessed/test_data.npy")
@@ -24,29 +25,36 @@ test_labels = np.load("/storage/hpc_anna/GMiC/Data/ECoG/preprocessed/test_labels
 # split the training data into two halves
 #   fh stands for first half
 #   sh stands for second half
+print "Splitting data in two halves..."
 fh_data, fh_labels, sh_data, sh_labels = DataHandler.split(0.5, train_data, train_labels)
 
 # train HMM on first 50% of the training set
+print "Training HMM classifier..."
 hmmcl = HMMClassifier()
 model_pos, model_neg = hmmcl.train(NSTATES, NITERS, fh_data, fh_labels)
 
 # feed second 50% of the training set into the HMM to obtain
 # pos/neg ratio for every sequence in the second half of the training set
+print "Extracting ratios based on the HMM model..."
 sh_ratios = hmmcl.pos_neg_ratios(model_pos, model_neg, sh_data)
 test_ratios = hmmcl.pos_neg_ratios(model_pos, model_neg, test_data)
 
 # apply fourier transform on the second 50% of the training set
+print "Fouriering the second half of the dataset..."
 fourier_sh_data = Fourier.data_to_fourier(sh_data)
 fourier_test_data = Fourier.data_to_fourier(test_data)
 
 # augment fourier results of the second 50% train with the ratios thus producing an enriched dataset
+print "Merging Fourier features and HMM-based ratios..."
 enriched_sh_data = np.hstack((fourier_sh_data, sh_ratios.reshape(len(sh_ratios), 1)))
 enriched_test_data = np.hstack((fourier_test_data, test_ratios.reshape(len(test_ratios), 1)))
 
 # train RF on the enriched dataset
+print "Training RF on the merged dataset..."
 rf = RandomForestClassifier(n_estimators=500)
 rf.fit(enriched_sh_data, sh_labels)
 
 # test RF on the test set
+print "Testing the final model..."
 print str(rf.score(enriched_sh_data, sh_labels)) + " - accuracy on train data"
 print str(rf.score(enriched_test_data, test_labels)) + " - accuracy on test data"
--- RF-LSTM/rflstm_on_ecog.py
+++ RF-LSTM/rflstm_on_ecog.py
@@ -12,9 +12,10 @@ from LSTM.lstm_classifier import LSTMClassifier
 from sklearn.ensemble import RandomForestClassifier
 
 # parameters
-NEPOCHS = 10
+NEPOCHS = 1
 
 # load the data
+print "Reading data..."
 train_data = np.load("/storage/hpc_anna/GMiC/Data/ECoG/preprocessed/train_data.npy")
 train_labels = np.load("/storage/hpc_anna/GMiC/Data/ECoG/preprocessed/train_labels.npy")
 test_data = np.load("/storage/hpc_anna/GMiC/Data/ECoG/preprocessed/test_data.npy")
@@ -23,29 +24,36 @@ test_labels = np.load("/storage/hpc_anna/GMiC/Data/ECoG/preprocessed/test_labels
 # split the training data into two halves
 #   fh stands for first half
 #   sh stands for second half
+print "Splitting data in two halves..."
 fh_data, fh_labels, sh_data, sh_labels = DataHandler.split(0.5, train_data, train_labels)
 
 # train LSTM on first 50% of the training set
-lstmcl = LSTMClassifier(256, 0.5, 'rmsprop', NEPOCHS)
+print "Training LSTM classifier..."
+lstmcl = LSTMClassifier(2000, 0.8, 'adagrad', NEPOCHS)
 model_pos, model_neg = lstmcl.train(fh_data, fh_labels)
 
 # feed second 50% of the training set into the LSTM to obtain
 # pos/neg ratio for every sequence in the second half of the training set
+print "Extracting ratios based on the LSTM model..."
 sh_ratios = lstmcl.pos_neg_ratios(model_pos, model_neg, sh_data)
 test_ratios = lstmcl.pos_neg_ratios(model_pos, model_neg, test_data)
 
 # apply fourier transform on the second 50% of the training set
+print "Fouriering the second half of the dataset..."
 fourier_sh_data = Fourier.data_to_fourier(sh_data)
 fourier_test_data = Fourier.data_to_fourier(test_data)
 
 # augment fourier results of the second 50% train with the ratios thus producing an enriched dataset
+print "Merging Fourier features and HMM-based ratios..."
 enriched_sh_data = np.hstack((fourier_sh_data, sh_ratios.reshape(len(sh_ratios), 1)))
 enriched_test_data = np.hstack((fourier_test_data, test_ratios.reshape(len(test_ratios), 1)))
 
 # train RF on the enriched dataset
+print "Training RF on the merged dataset..."
 rf = RandomForestClassifier(n_estimators=500)
 rf.fit(enriched_sh_data, sh_labels)
 
 # test RF on the test set
+print "Testing the final model..."
 print str(rf.score(enriched_sh_data, sh_labels)) + " - accuracy on train data"
 print str(rf.score(enriched_test_data, test_labels)) + " - accuracy on test data"
