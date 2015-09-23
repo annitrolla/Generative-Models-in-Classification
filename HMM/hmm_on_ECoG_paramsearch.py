e8362eea59617e217bed2d2aa2edca8d3b126c71
kuz <ilya.kuzovkin@gmail.com>
2015-09-23 22:37:01 +0300

    (rename)

---
+++ HMM/hmm_on_ECoG_paramsearch.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 22 17:58:39 2015
+
+@author: annaleontjeva
+"""
+from HMM.hmm_classifier import HMMClassifier
+from DataNexus.datahandler import DataHandler
+import numpy as np
+
+train_data = np.load("/storage/hpc_anna/GMiC/Data/ECoG/preprocessed/train_data.npy")
+train_labels = np.load("/storage/hpc_anna/GMiC/Data/ECoG/preprocessed/train_labels.npy")
+test_data = np.load("/storage/hpc_anna/GMiC/Data/ECoG/preprocessed/test_data.npy")
+test_labels = np.load("/storage/hpc_anna/GMiC/Data/ECoG/preprocessed/test_labels.npy")
+
+hmmcl = HMMClassifier()
+#model_pos, model_neg = hmmcl.train(20, 100, train_data, train_labels)
+#print hmmcl.test_model(model_pos, model_neg, test_data, test_labels)
+hmmcl.find_best_parameter(0.7, range(20,31), 10, 5, train_data, train_labels)
