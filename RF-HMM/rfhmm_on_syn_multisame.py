4b60037292de7fc31ff9ecf85cd27f41b1298f27
kuz <ilya.kuzovkin@gmail.com>
2015-10-04 23:33:50 +0300

    Synthetic data generation now goes via generate data -> store -> run models pipeline (as if the generated data is a real dataset from the outside world)

--- RF-HMM/rfhmm_on_syn_multisame.py
+++ RF-HMM/rfhmm_on_syn_multisame.py
@@ -14,75 +14,21 @@ from sklearn.ensemble import RandomForestClassifier
 from HMM.hmm_classifier import HMMClassifier
 
 
-# parameters
-nsamples = 10000
-nq = nsamples / 4
-nfeatures = 10
-nseqfeatures = 20
-seqlen = 30
-
-s_pos_mu = 0.0
-s_pos_sd = 1.0
-s_neg_mu = 5.0
-s_neg_sd = 1.0
-
-
 #
-# Generate data
+# Load the dataset
 #
-
-# generate labels
-labels = np.array([1] * nq + [0] * nq + [1] * nq + [0] * nq)
-
-# generate static features
-static = np.vstack((np.random.normal(5.0, 1.0, (nq, nfeatures)),
-                    np.random.normal(0.0, 1.0, (nq, nfeatures)),
-                    np.random.normal(0.0, 1.0, (nq, nfeatures)),
-                    np.random.normal(0.0, 1.0, (nq, nfeatures))))
-
-# generate dynamic features
-coefs_q1 = np.random.normal(0.2, 1, seqlen - 1)
-coefs_q2 = np.random.normal(0.2, 1, seqlen - 1)
-coefs_q3 = np.random.normal(0.0, 1, seqlen - 1)
-coefs_q4 = np.random.normal(0.2, 1, seqlen - 1)
-dynamic_q1 = np.empty((nq, nseqfeatures, seqlen))
-dynamic_q2 = np.empty((nq, nseqfeatures, seqlen))
-dynamic_q3 = np.empty((nq, nseqfeatures, seqlen))
-dynamic_q4 = np.empty((nq, nseqfeatures, seqlen))
-for i in range(nq):
-    dynamic_q1[i, :, 0] = np.random.uniform(-1.0, 1.0, nseqfeatures)
-    dynamic_q2[i, :, 0] = np.random.uniform(-1.0, 1.0, nseqfeatures)
-    dynamic_q3[i, :, 0] = np.random.uniform(-1.0, 1.0, nseqfeatures)
-    dynamic_q4[i, :, 0] = np.random.uniform(-1.0, 1.0, nseqfeatures)
-for s in range(nq):
-    for t in range(1, seqlen):
-        dynamic_q1[s, :, t] = coefs_q1[t - 1] * dynamic_q1[s, :, t - 1] 
-        dynamic_q2[s, :, t] = coefs_q2[t - 1] * dynamic_q2[s, :, t - 1]
-        dynamic_q3[s, :, t] = coefs_q3[t - 1] * dynamic_q3[s, :, t - 1]
-        dynamic_q4[s, :, t] = coefs_q4[t - 1] * dynamic_q4[s, :, t - 1]
-dynamic = np.vstack((dynamic_q1, dynamic_q2, dynamic_q3, dynamic_q4))
-
-
-#
-# Split the dataset into training and validation
-#
-
-# pick samples for training and for validation
-train_idx = np.random.choice(range(0, nsamples), size=np.round(nsamples * 0.7, 0), replace=False)
-val_idx = list(set(range(0, nsamples)) - set(train_idx))
-
-static_train = static[train_idx, :]
-dynamic_train = dynamic[train_idx, :, :]
-static_val = static[val_idx, :]
-dynamic_val = dynamic[val_idx, :, :]
-labels_train = labels[train_idx]
-labels_val = labels[val_idx]
+print 'Loading the dataset..'
+static_train = np.load('/storage/hpc_anna/GMiC/Data/syn_multisame/train_static.npy')
+dynamic_train = np.load('/storage/hpc_anna/GMiC/Data/syn_multisame/train_dynamic.npy')
+static_val = np.load('/storage/hpc_anna/GMiC/Data/syn_multisame/test_static.npy')
+dynamic_val = np.load('/storage/hpc_anna/GMiC/Data/syn_multisame/test_dynamic.npy')
+labels_train = np.load('/storage/hpc_anna/GMiC/Data/syn_multisame/train_labels.npy')
+labels_val = np.load('/storage/hpc_anna/GMiC/Data/syn_multisame/test_labels.npy')
 
 
 #
 # Sanity Checks
 #
-
 print "Expected performance of a lonely model is 0.75, of the joint model 1.0"
 
 # a) static data classification
@@ -99,8 +45,6 @@ print "HMM with dynamic features on validation set: %.4f" % hmmcl.test(model_pos
 #
 # Evaluating Joint Model
 #
-
-print ""
 print "Evaluating joint model:"
 print "Splitting data in two halves..."
 fh_idx = np.random.choice(range(0, dynamic_train.shape[0]), size=np.round(dynamic_train.shape[0] * 0.5, 0), replace=False)
