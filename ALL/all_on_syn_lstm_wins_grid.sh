c6457b769d5ad47df36342441062ec7402b07cdf
kuz <ilya.kuzovkin@gmail.com>
2015-10-17 20:10:54 +0300

    In the grid search run each parameter combination as a separate process, so that GPU driver could have chance to free memory

---
+++ ALL/all_on_syn_lstm_wins_grid.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+RUN=0
+while IFS=, read nsamples nfeatures nseqfeatures seqlen
+do
+    echo "------------------------- Run $RUN -------------------------"
+    python all_on_syn_lstm_wins_grid.py -n $nsamples -s $nfeatures -d $nseqfeatures -l $seqlen
+    let RUN=RUN+1
+done < grid.txt
---
+++ ALL/produce_parameter_grid.py
@@ -0,0 +1,25 @@
+"""
+
+Generates parameter search grid from the list of paramteres and
+stores it to file for bash script to pick up
+
+"""
+
+import numpy as np
+import itertools
+
+def expandgrid(*itrs):
+    product = list(itertools.product(*itrs))
+    return [[x[i] for x in product] for i in range(len(itrs))]
+
+nsamples = [500, 1000, 1500, 2000, 2500, 3000]
+nfeatures = [1, 5, 10, 20, 30]
+nseqfeatures = [1, 5, 10, 20, 30]
+seqlen = [3, 10, 30, 50]
+
+prs = np.array(expandgrid(nsamples, nfeatures, nseqfeatures, seqlen)).T
+with open('grid.txt', 'w') as f:
+    for p in prs:
+        f.write('%d, %d, %d, %d\n' % (p[0], p[1], p[2], p[3]))
+f.close()
+
