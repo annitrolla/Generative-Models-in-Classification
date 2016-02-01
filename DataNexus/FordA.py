fbda9daf5cd7f0d0a809e9968d78496905e4db41
kuz <ilya.kuzovkin@gmail.com>
2016-02-01 20:36:03 +0200

    Combine spatialized data with dynamic features on FordA example

--- DataNexus/FordA.py
+++ DataNexus/FordA.py
@@ -10,10 +10,12 @@ all_raw = np.concatenate((train_raw, test_raw))
 print 'Transforming data...'
 labels = all_raw[:, 0].astype(int);
 labels[labels == -1] = 0
-dynamic = all_raw[:, 1:].reshape(all_raw.shape[0], 1, all_raw.shape[1] - 1)
+static = all_raw[:, 1:]
+dynamic = static.reshape(all_raw.shape[0], 1, all_raw.shape[1] - 1)
 
 # store data
 print 'Storing data...'
+np.save('/storage/hpc_anna/GMiC/Data/FordA/preprocessed/train_static.npy', static)
 np.save('/storage/hpc_anna/GMiC/Data/FordA/preprocessed/train_dynamic.npy', dynamic)
 np.save('/storage/hpc_anna/GMiC/Data/FordA/preprocessed/train_labels.npy', labels)
 
