c16e582d42ddd51a1a29e01ec7d1daa8ff6a67fa
kuz <ilya.kuzovkin@gmail.com>
2016-02-03 17:24:18 +0200

    Hybrid and Ensemble on Splice data

--- DataNexus/splice.py
+++ DataNexus/splice.py
@@ -1,7 +1,6 @@
 import numpy as np
 from sklearn.preprocessing import OneHotEncoder
 
-
 # load the data
 print 'Loading data...'
 train = np.loadtxt('/storage/hpc_anna/GMiC/Data/SpliceData/raw/splice_train.csv', delimiter=',')
@@ -14,15 +13,18 @@ labels = labels.astype(int)
 # one-hot encoding
 enc = OneHotEncoder(sparse=False, n_values=8)
 train_encoded = enc.fit_transform(train)
-slonopotam = np.transpose(train_encoded.reshape((1535,60,8)), axes=(0,2,1))
+dynamic = np.transpose(train_encoded.reshape((1535,60,8)), axes=(0,2,1))
+static = dynamic.reshape((dynamic.shape[0], dynamic.shape[1] * dynamic.shape[2]))
 
 # shuffle
 print 'Shuffling...'
 new_train_idx = np.random.choice(range(0, nsamples), size=nsamples, replace=False)
-slonopotam = slonopotam[new_train_idx]
+dynamic = dynamic[new_train_idx]
+static = static[new_train_idx]
 labels = labels[new_train_idx]
 
 # store data
 print 'Storing data...'
-np.save('/storage/hpc_anna/GMiC/Data/SpliceData/preprocessed/train_dynamic.npy', slonopotam)
+np.save('/storage/hpc_anna/GMiC/Data/SpliceData/preprocessed/train_dynamic.npy', dynamic)
+np.save('/storage/hpc_anna/GMiC/Data/SpliceData/preprocessed/train_static.npy', static)
 np.save('/storage/hpc_anna/GMiC/Data/SpliceData/preprocessed/train_labels.npy', labels)
