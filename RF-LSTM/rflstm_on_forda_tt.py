9d3204a08d61d03accf6171b0ca469ff2e419d8d
kuz <ilya.kuzovkin@gmail.com>
2016-02-10 22:48:30 +0200

    FordA

--- RF-LSTM/rflstm_on_forda_tt.py
+++ RF-LSTM/rflstm_on_forda_tt.py
@@ -9,14 +9,11 @@ from LSTM.lstm_classifier import LSTMClassifier
 #
 nfolds = 5
 nestimators = 500
-nhmmstates = 2
-nhmmiter = 50
-hmmcovtype = "full"  # options: full, diag, spherical
 lstmsize = 512
 lstmdropout = 0.0
 lstmoptim = 'rmsprop'
 lstmnepochs = 20
-lstmbatchsize = 32
+lstmbatchsize = 1
 
 
 #
@@ -24,16 +21,15 @@ lstmbatchsize = 32
 #
 print 'Loading the dataset..'
 
-# YES, THEY ARE SWAPPED
-test_static = np.load('/storage/hpc_anna/GMiC/Data/FordA/preprocessed/train_static.npy')
-test_dynamic = np.load('/storage/hpc_anna/GMiC/Data/FordA/preprocessed/train_dynamic.npy')
-test_labels = np.load('/storage/hpc_anna/GMiC/Data/FordA/preprocessed/train_labels.npy')
-test_nsamples = test_static.shape[0]
-
-train_static = np.load('/storage/hpc_anna/GMiC/Data/FordA/preprocessed/test_static.npy')
-train_dynamic = np.load('/storage/hpc_anna/GMiC/Data/FordA/preprocessed/test_dynamic.npy')
-train_labels = np.load('/storage/hpc_anna/GMiC/Data/FordA/preprocessed/test_labels.npy')
+train_static = np.load('/storage/hpc_anna/GMiC/Data/FordA/preprocessed/train_static.npy')
+train_dynamic = np.load('/storage/hpc_anna/GMiC/Data/FordA/preprocessed/train_dynamic.npy')
+train_labels = np.load('/storage/hpc_anna/GMiC/Data/FordA/preprocessed/train_labels.npy')
 train_nsamples = train_static.shape[0]
+
+test_static = np.load('/storage/hpc_anna/GMiC/Data/FordA/preprocessed/test_static.npy')
+test_dynamic = np.load('/storage/hpc_anna/GMiC/Data/FordA/preprocessed/test_dynamic.npy')
+test_labels = np.load('/storage/hpc_anna/GMiC/Data/FordA/preprocessed/test_labels.npy')
+test_nsamples = test_static.shape[0]
 
 
 #
@@ -61,14 +57,6 @@ predictions_trainB_rf[predictions_trainB_rf == -inf] = np.min(predictions_trainB
 predictions_test_rf = rf.predict_log_proba(test_static)
 predictions_test_rf[predictions_test_rf == -inf] = np.min(predictions_test_rf[predictions_test_rf != -inf])
 
-# extract predictions using HMM on dynamic
-hmmcl = HMMClassifier()
-model_pos, model_neg = hmmcl.train(nhmmstates, nhmmiter, hmmcovtype, trainA_dynamic, trainA_labels)
-predictions_trainB_hmm = hmmcl.predict_log_proba(model_pos, model_neg, trainB_dynamic)
-ratios_trainB_hmm = hmmcl.pos_neg_ratios(model_pos, model_neg, trainB_dynamic)
-predictions_test_hmm = hmmcl.predict_log_proba(model_pos, model_neg, test_dynamic)
-ratios_test_hmm = hmmcl.pos_neg_ratios(model_pos, model_neg, test_dynamic)
-
 # extract predictions using LSTM on dynamic
 lstmcl = LSTMClassifier(lstmsize, lstmdropout, lstmoptim, lstmnepochs, lstmbatchsize)
 model_pos, model_neg = lstmcl.train(trainA_dynamic, trainA_labels)
@@ -85,14 +73,10 @@ ratios_test_lstm = lstmcl.pos_neg_ratios(model_pos, model_neg, test_dynamic)
 #
 
 # datasets for ensemble learning
-trainB_predictions_combined_rf_hmm = np.concatenate((predictions_trainB_rf, ratios_trainB_hmm.reshape((ratios_trainB_hmm.shape[0], 1))), axis=1)
-test_predictions_combined_rf_hmm = np.concatenate((predictions_test_rf, ratios_test_hmm.reshape((ratios_test_hmm.shape[0], 1))), axis=1)
 trainB_predictions_combined_rf_lstm = np.concatenate((predictions_trainB_rf, ratios_trainB_lstm.reshape((ratios_trainB_lstm.shape[0], 1))), axis=1)
 test_predictions_combined_rf_lstm = np.concatenate((predictions_test_rf, ratios_test_lstm.reshape((ratios_test_lstm.shape[0], 1))), axis=1)
 
 # datasets for hybrid learning
-trainB_enriched_by_hmm = np.concatenate((trainB_static, np.matrix(ratios_trainB_hmm).T), axis=1)
-test_enriched_by_hmm = np.concatenate((test_static, np.matrix(ratios_test_hmm).T), axis=1)
 trainB_enriched_by_lstm = np.concatenate((trainB_static, np.matrix(ratios_trainB_lstm).T), axis=1)
 test_enriched_by_lstm = np.concatenate((test_static, np.matrix(ratios_test_lstm).T), axis=1)
 
@@ -121,52 +105,20 @@ test_static_and_dynamic_as_static = np.concatenate((test_static, test_dynamic_as
 #
 scores = {1: [], 2: [], 3: [], 4: [], 5: [], 6: [], 7: [], 8: [], 9: [], 10: [], 11: []}
 
-# Ensemble on predictions by RF and HMM (1)
-rf = RandomForestClassifier(n_estimators=nestimators)
-rf.fit(trainB_predictions_combined_rf_hmm, trainB_labels)
-scores[1].append(rf.score(test_predictions_combined_rf_hmm, test_labels))
-
 # Ensemble on predictions by RF and LSTM (2)
 rf = RandomForestClassifier(n_estimators=nestimators)
 rf.fit(trainB_predictions_combined_rf_lstm, trainB_labels)
 scores[2].append(rf.score(test_predictions_combined_rf_lstm, test_labels))
-
-# Hybrid on features enriched by HMM (3)
-rf = RandomForestClassifier(n_estimators=nestimators)
-rf.fit(trainB_enriched_by_hmm, trainB_labels)
-scores[3].append(rf.score(test_enriched_by_hmm, test_labels))
 
 # Hybrid on features enriched by LSTM (4)
 rf = RandomForestClassifier(n_estimators=nestimators)
 rf.fit(trainB_enriched_by_lstm, trainB_labels)
 scores[4].append(rf.score(test_enriched_by_lstm, test_labels))
 
-# RF on static features (5)
-rf = RandomForestClassifier(n_estimators=nestimators)
-rf.fit(trainB_static, trainB_labels)
-scores[5].append(rf.score(test_static, test_labels))
-
-# RF on dynamic features (6)
-rf = RandomForestClassifier(n_estimators=nestimators)
-rf.fit(trainB_dynamic_as_static, trainB_labels)
-scores[6].append(rf.score(test_dynamic_as_static, test_labels))
-
-# HMM on dynamic features (7)
-hmmcl = HMMClassifier()
-model_pos, model_neg = hmmcl.train(nhmmstates, nhmmiter, hmmcovtype, trainB_dynamic, trainB_labels)
-acc, auc = hmmcl.test(model_pos, model_neg, test_dynamic, test_labels)
-scores[7].append(acc)
-
 # LSTM on dynamic features (8)
 lstmcl = LSTMClassifier(lstmsize, lstmdropout, lstmoptim, lstmnepochs, lstmbatchsize)
 model_pos, model_neg = lstmcl.train(trainB_dynamic, trainB_labels)
 scores[8].append(lstmcl.test(model_pos, model_neg, test_dynamic, test_labels))
-
-# HMM on dynamic and static (turned into fake sequences) (9)
-#hmmcl = HMMClassifier()
-#model_pos, model_neg = hmmcl.train(nhmmstates, nhmmiter, hmmcovtype, trainB_dynamic_and_static_as_dynamic, trainB_labels_all)
-#acc, auc = hmmcl.test(model_pos, model_neg, test_dynamic_and_static_as_dynamic, test_labels)
-#scores[9].append(acc)
 
 # LSTM on dynamic and static (turned into fake sequences) (10)
 #lstmcl = LSTMClassifier(lstmsize, lstmdropout, lstmoptim, lstmnepochs, lstmbatchsize)
@@ -178,15 +130,9 @@ scores[8].append(lstmcl.test(model_pos, model_neg, test_dynamic, test_labels))
 #rf.fit(trainB_static_and_dynamic_as_static, trainB_labels_all)
 #scores[11].append(rf.score(test_static_and_dynamic_as_static, test_labels_all))
 
-print "===> (1) Ensemble (RF) on predictions by RF and HMM: %.4f (+/- %.4f) %s" % (np.mean(scores[1]), np.std(scores[1]), scores[1])
 print "===> (2) Ensemble (RF) on predictions by RF and LSTM: %.4f (+/- %.4f) %s" % (np.mean(scores[2]), np.std(scores[2]), scores[2])
-print "===> (3) Hybrid (RF) on features enriched by HMM: %.4f (+/- %.4f) %s" % (np.mean(scores[3]), np.std(scores[3]), scores[3])
 print "===> (4) Hybrid (RF) on features enriched by LSTM: %.4f (+/- %.4f) %s" % (np.mean(scores[4]), np.std(scores[4]), scores[4])
-print "===> (5) RF on static features: %.4f (+/- %.4f) %s" % (np.mean(scores[5]), np.std(scores[5]), scores[5])
-print "===> (6) RF on dynamic (spatialized) features: %.4f (+/- %.4f) %s" % (np.mean(scores[6]), np.std(scores[6]), scores[6])
-print "===> (7) HMM on dynamic features: %.4f (+/- %.4f) %s" % (np.mean(scores[7]), np.std(scores[7]), scores[7])
 print "===> (8) LSTM on dynamic features: %.4f (+/- %.4f) %s" % (np.mean(scores[8]), np.std(scores[8]), scores[8])
-#print "===> (9) HMM on dynamic and static features: %.4f (+/- %.4f) %s" % (np.mean(scores[9]), np.std(scores[9]), scores[9])
 #print "===> (10) LSTM on dynamic features: %.4f (+/- %.4f) %s" % (np.mean(scores[10]), np.std(scores[10]), scores[10])
 #print "===> (11) RF on dynamic (spatialized) features: %.4f (+/- %.4f) %s" % (np.mean(scores[11]), np.std(scores[11]), scores[11])
 
---
+++ RF-LSTM/rflstm_on_forda_tt_activations.py
@@ -0,0 +1,88 @@
+import numpy as np
+from numpy import inf
+from sklearn.ensemble import RandomForestClassifier
+from HMM.hmm_classifier import HMMClassifier
+from LSTM.lstm_classifier import LSTMClassifier 
+
+#
+# General parameters
+#
+nfolds = 5
+nestimators = 500
+lstmsize = 512
+lstmdropout = 0.0
+lstmoptim = 'rmsprop'
+lstmnepochs = 20
+lstmbatchsize = 1
+
+
+#
+# Load the dataset
+#
+print 'Loading the dataset..'
+
+train_static = np.load('/storage/hpc_anna/GMiC/Data/FordA/preprocessed/train_static.npy')
+train_dynamic = np.load('/storage/hpc_anna/GMiC/Data/FordA/preprocessed/train_dynamic.npy')
+train_labels = np.load('/storage/hpc_anna/GMiC/Data/FordA/preprocessed/train_labels.npy')
+train_nsamples = train_static.shape[0]
+
+test_static = np.load('/storage/hpc_anna/GMiC/Data/FordA/preprocessed/test_static.npy')
+test_dynamic = np.load('/storage/hpc_anna/GMiC/Data/FordA/preprocessed/test_dynamic.npy')
+test_labels = np.load('/storage/hpc_anna/GMiC/Data/FordA/preprocessed/test_labels.npy')
+test_nsamples = test_static.shape[0]
+
+
+#
+# Split training
+#
+train_half = train_nsamples / 2
+trainA_static = train_static[:train_half]
+trainB_static = train_static[train_half:]
+trainA_dynamic = train_dynamic[:train_half]
+trainB_dynamic = train_dynamic[train_half:]
+trainA_labels = train_labels[:train_half]
+trainB_labels = train_labels[train_half:]
+
+
+#
+# Train enrichment models on trainA
+#
+print 'Training enrichment models...'
+
+# train LSTM activations extractor
+lstmcl = LSTMClassifier(lstmsize, lstmdropout, lstmoptim, lstmnepochs, lstmbatchsize)
+model_pos, model_neg = lstmcl.train(trainA_dynamic, trainA_labels)
+
+trainB_activations_pos = lstmcl.activations(model_pos, trainB_dynamic)
+trainB_activations_neg = lstmcl.activations(model_neg, trainB_dynamic)
+trainB_activations = np.concatenate((trainB_activations_pos[:, 499, :], trainB_activations_neg[:, 499, :]), axis=1)
+
+test_activations_pos = lstmcl.activations(model_pos, test_dynamic)
+test_activations_neg = lstmcl.activations(model_neg, test_dynamic)
+test_activations = np.concatenate((test_activations_pos[:, 499, :], test_activations_neg[:, 499, :]), axis=1)
+
+#
+# Prepare combined datasets for the future experiments
+#
+
+# datasets for hybrid learning
+trainB_enriched_by_lstm = np.concatenate((trainB_static, trainB_activations), axis=1)
+test_enriched_by_lstm = np.concatenate((test_static, test_activations), axis=1)
+
+
+#
+# Training models on trainB and performance estimation on test
+#
+scores = {1: [], 2: [], 3: [], 4: [], 5: [], 6: [], 7: [], 8: [], 9: [], 10: [], 11: [], 12: []}
+
+# Hybrid on static features and LSTM activations (12)
+rf = RandomForestClassifier(n_estimators=nestimators)
+rf.fit(trainB_enriched_by_lstm, trainB_labels)
+scores[12].append(rf.score(test_enriched_by_lstm, test_labels))
+
+
+#
+# Print out the results
+#
+print "===> (12) Hybrid (RF) on features + LSTM activations: %.4f (+/- %.4f) %s" % (np.mean(scores[12]), np.std(scores[12]), scores[12])
+
